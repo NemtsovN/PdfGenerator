2a2b82a7d1ef9662572d50874c14eddb9e233f38
NemtsovN <n.nemtsov@andersenlab.com>
2023-06-26 20:47:55 +0200

    Add configuration properties for PuppeteerSharp

--- prototypes/dotNet/src/PuppeteerSharpGenerator/PdfGenerator.cs
+++ prototypes/dotNet/src/PuppeteerSharpGenerator/PdfGenerator.cs
@@ -1,4 +1,5 @@
 using PuppeteerSharp;
+using PuppeteerSharp.Media;
 
 namespace PdfGenerator.Prototypes.PuppeteerSharpGenerator;
 
@@ -18,6 +19,15 @@ public static class PdfGenerator
         await using var browser = await Puppeteer.LaunchAsync(browserLaunchOptions);
         await using var page = await browser.NewPageAsync();
 
+        // Set a4 page size
+        await page.SetViewportAsync(new ViewPortOptions
+        {
+            Width = 2480,
+            Height = 3508
+        });
+        
+        await page.EmulateMediaTypeAsync(MediaType.Print);
+        
         // Inject HTML
         await page.SetContentAsync(htmlInput);
 
@@ -26,8 +36,21 @@ public static class PdfGenerator
         // Generate PDF file from provided Puppeteer page
         var pdfOptions = new PdfOptions
         {
+            Format = PaperFormat.A4,
             DisplayHeaderFooter = true,
-            PrintBackground = true
+            PrintBackground = true,
+            MarginOptions = new()
+            {
+                Bottom = "1.5cm",
+                Left = "1.5cm",
+                Right = "1.5cm",
+                Top = "1.5cm"
+            },
+            // Width = "2480px",
+            // Height = "3508px",
+            Scale = new decimal(0.99),
+            HeaderTemplate = "<div id=\"footer-template\" style=\"font-size:10px !important; color:#808080;\">Header template text</div>",
+            FooterTemplate = "<div id=\"footer-template\" style=\"font-size:10px !important; color:#808080;\">Footer template text</div>"
         };
 
         var resultPdf = await page.PdfStreamAsync(pdfOptions);
